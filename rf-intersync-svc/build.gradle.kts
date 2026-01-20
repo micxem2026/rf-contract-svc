2cc7f0c8f864380a6502266b757a2cd4cb9e62de
Emelyanov Michael <micxem@yandex.ru>
2026-01-20 16:38:37 +0300

    добавил поле code_1c в организации и контрагенты

--- rf-intersync-svc/build.gradle.kts
+++ rf-intersync-svc/build.gradle.kts
@@ -2,8 +2,13 @@ import org.gradle.kotlin.dsl.withType
 import org.jetbrains.kotlin.gradle.dsl.JvmTarget
 import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
 
-kotlin {
+/*kotlin {
     jvmToolchain(17)
+}*/
+
+java {
+    sourceCompatibility = JavaVersion.VERSION_17
+    targetCompatibility = JavaVersion.VERSION_17
 }
 
 repositories {
